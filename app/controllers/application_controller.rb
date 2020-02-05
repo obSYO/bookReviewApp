bc8594c822287d5d794abfe5279a77e49c57ff7c
obSYO <sho020202forest@gmail.com>
2020-02-05 15:15:37 +0900

    認証情報をcredentialに記述、applicationcontrollerにbasic認証を本番環境で動作させるように機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
   def index
   end
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic_auth[:username] && password == Rails.application.credentials.basic_auth[:password]
+    end
   end
 end
